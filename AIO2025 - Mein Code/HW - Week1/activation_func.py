a5a0b0992194852f676a17e75c28db83873fc209
Sukmadi <54486758+MathematicGuy@users.noreply.github.com>
2025-06-11 14:23:55 +0700

    update activation function and testing recursion

--- AIO2025 - Mein Code/HW - Week1/activation_func.py
+++ AIO2025 - Mein Code/HW - Week1/activation_func.py
@@ -1,6 +1,10 @@
 """Calculating Activation Functions like ReLU, Softmax, Sigmoid, ELU
 """
 
+import math
+import random
+import numpy as np
+
 def is_int(*args):
     """Check if any value in *args is int, if not return False flag
     Returns:
@@ -49,4 +53,165 @@ def evaluate_f1_components(tp, fp, fn):
         return None
 
 
-evaluate_f1_components(tp=2, fp=3, fn=4.4)
+def sigmoid(x):
+    return 1 / (1+math.e**(-x))
+
+def relu(x):
+    #? Add this to global func later
+    if x <= 0:
+        return 0
+    else:
+        return x
+
+
+def elu(x, alpha=0.05):
+    if x <= 0:
+        return alpha*(math.e**x - 1)
+    else:
+        return x
+
+activations = {
+    'sigmoid': sigmoid,
+    'relu': relu,
+    'elu': elu,
+}
+
+def interactive_activation_function(x=5, input_activation='sigmoid'):
+    input_activation = input(str('Input activation Function (sigmoid|relu|elu):'))
+
+    if not isinstance(x, (int, float)):
+        return "Warning, x must be a number, not anything else"
+
+    activation = activations.get(input_activation)
+
+    if activation: # if activation exist
+        print(f'Input x = {x}')
+        print(f'{input_activation}: f({x}) = {activation(x)}')
+    elif not activation:
+        print(f'{input_activation} is not supported')
+    else:
+        raise ValueError("Invalid Operation")
+
+
+def mae(y, y_hat, n):
+    y = np.array(y)
+    y_hat = np.array(y_hat)
+
+    print('y_hat:', y_hat)
+
+    return np.sum(np.abs(y - y_hat)) / n
+
+
+def mse(y, y_hat, n):
+    y = np.array(y)
+    y_hat = np.array(y_hat)
+
+    print('y:', y)
+    print('y_hat:', y_hat)
+
+    return sum((y - y_hat)**2) / n
+
+
+def rmse(y, y_hat, n):
+    y = np.array(y)
+    y_hat = np.array(y_hat)
+    print('y_hat:', y_hat)
+
+    return math.sqrt(sum((y - y_hat)**2) / n)
+
+
+loss_functions = {
+    'mae': mae,
+    'mse': mse,
+    'rmse': rmse,
+}
+
+def interactive_loss_function(input_loss_func='mse'):
+    """calculate user input loss function
+
+    Args:
+        input_loss_func (str, optional): loss function name. Defaults to 'mse'.
+
+    Raises:
+        ValueError: if num_sample is not integer
+
+    Returns:
+        loss_name: loss function name
+        sample: sample_i
+        predict predicted_value_i
+        target: target_value_i
+        loss: loss_i return from loss_function
+    """
+
+    num_samples_str = input("Input number of samples (integer number) which are generated:")
+
+    if not num_samples_str.isnumeric():
+        return "number of samples must be an integer number"
+
+    num_samples = int(num_samples_str)
+    input_loss_func = input('Enter loss function (mae|mse|rmse):')
+
+    #? samples = (target_i, predict_i) - [y, y_hat]
+    y = [random.uniform(0, 10) for _ in range(num_samples)]
+    y_pred = [random.uniform(0, 10) for _ in range(num_samples)]
+    n = len(y)
+
+
+    loss_func = loss_functions.get(input_loss_func)
+
+    if loss_func: # if loss_func exist
+        loss_result = loss_func(y, y_pred, n)
+        # print(f'resulkt: {loss_result}')
+
+        #? Print result
+        for i in range(num_samples):
+            print(f'loss_name: {input_loss_func.upper()}, sample: {i}, pred: {y_pred[i]},'
+                    f' target: {y[i]}')
+
+        print(f'Final {input_loss_func.upper()}: {np.sum(loss_result)}')
+
+    elif not loss_func:
+        print(f'{input_loss_func} is not supported')
+    else:
+        raise ValueError("Invalid Operation")
+
+def factorial(div):
+    return factorial(div - 1)
+
+
+def sin(x, n):
+    x = np.array(x)
+    div = (2*n + 1)
+    result = 0
+    for i in range(n):
+        result += ((-1)**n) * (x**(2*n  + 1)) / factorial(div)
+
+
+def cos(x, n):
+    pass
+
+def sinh(x, n):
+    pass
+
+def cosh(x, n):
+    pass
+
+evaluation_func = {
+    'sin': sin,
+    'cos': mse,
+    'sinh': rmse,
+    'cosh': cosh
+}
+
+if __name__ == '__main__':
+    # evaluate_f1_components(tp=2, fp=3, fn=4.4)
+    # interactive_activation_function()
+    # interactive_loss_function()
+    x = 5
+    n = 10
+    eval_func = evaluation_func.get('sin')
+    if eval_func:
+        result = eval_func(x, n)
+        print(result)
+    else:
+        raise ValueError("Invalid Operation")
