08761a5d8d5a206cbfac4354ce9a530d5c96c3ff
Sukmadi <54486758+MathematicGuy@users.noreply.github.com>
2025-07-08 10:39:11 +0700

    organize code

---
+++ AIO2025 - Mein Code/Module_2/test.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# x = np.array([1,4,3])
+# y = np.ones_like(x)
+# result = np.dot(x, y)
+# print(result)
+
+x = np.array([1,4,3])
+result = np.sqrt(np.dot(x, x))
+print(result)
