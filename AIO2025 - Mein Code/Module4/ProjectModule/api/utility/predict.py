8a5b8e2820bab6ef01b642d7624c24708df7cb0b
MathematicGuy <nhatthanhdinh482@gmail.com>
2025-10-05 17:07:05 +0700

    update code from MacbokM5 with dvc

--- AIO2025 - Mein Code/Module4/ProjectModule/api/utility/predict.py
+++ AIO2025 - Mein Code/Module4/ProjectModule/api/utility/predict.py
@@ -45,7 +45,6 @@ def predict_single_sample(
             outputs, _ = fusion_model(numerical_input, cnn_input)
             probabilities = torch.softmax(outputs, dim=1)
             prediction = outputs.argmax(dim=1).item()
-            print('prediction:', prediction)
             disease_probability = probabilities[0, 1].item()
 
         print("\n" + "="*70)
