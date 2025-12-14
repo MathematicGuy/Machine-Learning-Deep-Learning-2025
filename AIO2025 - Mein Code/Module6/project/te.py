9e68576eb7195ca62c7e5b3e0f5ea0e5b0f581b6
MathematicGuy <nhatthanhdinh482@gmail.com>
2025-12-14 18:24:13 +0700

    add FPT Forecast preprocess

--- AIO2025 - Mein Code/Module6/project/te.py
+++
@@ -1,9 +0,0 @@
-import os
-import subprocess
-import gdown  # Ensure installed: pip install gdown
-
-# if not os.path.exists("FPT_train.csv"):
-#     gdown.download(id="1l2TtEaWrp4yieMDWE4Cmehnf5mLx3rop", output="FPT_train.csv")
-
-if not os.path.exists("FPT_train.csv"):
-    subprocess.run(["gdown", "--id", "1l2TtEaWrp4yieMDWE4Cmehnf5mLx3rop"], capture_output=True, text=True, check=True)
